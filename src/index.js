384d45f880116b4438354e641f018796e1906150
2m94 <71660630+2m94@users.noreply.github.com>
2023-09-05 19:28:59 -0400

    New Vans Component
    Vans component added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import Home from './pages/Home'
 import About from './pages/About'
+import Vans from './pages/Vans'
+
 
 function App() {
   return (
@@ -12,13 +14,16 @@ function App() {
     <header>
     <Link to="/">#VANLIFE</Link>
     <nav>
-      <Link to="/">Home</Link>
       <Link to="/about">About</Link>
+      <Link to="/Vans">Vans</Link>
+
     </nav>
     </header>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Vans" element={<Vans />} />
+
       </Routes>
     </BrowserRouter>
   )
